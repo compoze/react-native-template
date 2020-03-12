9ae7609170ca5c85381eebcac71083ab86a11569
CodyTroopDev <cody.troop@elko.dev>
2020-03-12 15:11:41 -0500

    installs react native elements and vector icons and fixes linking and duplicate imports

--- android/app/src/main/java/com/elko/myapp/MainApplication.java
+++ android/app/src/main/java/com/elko/myapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.elko.myapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
@@ -46,6 +47,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+                    new VectorIconsPackage(),
                     new RNFirebasePackage(),
                     new SafeAreaContextPackage(),
                     new RNFirebaseAuthPackage(),
