a36836de1c846a9226f76d6ea9aebe1a488ccb9d
CodyTroopDev <cody.troop@elko.dev>
2020-03-20 10:19:12 -0500

    created script to handle building env for different environments

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ Ensure that both XCode and Android Studio are setup
 
 For both Android and IOS be sure to run ```npm install``` this will install the required node_modules and pods for ios.
 
+### Changing Environments
+
+1. run ```npm run build <ENVIRONMENT_NAME>```
+2. if metro bundler is already running, stop it and run ```npm start```
+3. follow steps below for desired emulator
+
 ### Run IOS
 
 1. In a terminal, run ```npm run start```
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+
+ENV=$1
+PROD="prod"
+STAGE="stage"
+DEV="dev"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="$SCRIPT_DIR/.."
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+if [ "$ENV" != "$PROD" ] && [ "$ENV" != "$DEV" ] && [ "$ENV" != "$STAGE" ]
+then
+    echo "$ENV is not a valid environment, must be either $DEV, $STAGE, or $PROD"
+    exit 1
+else 
+    echo -e "${RED}Warning:${NC} Running this command replaces the entire .env, if you have information to keep there it must also be in the $ENV.env"
+    echo "Building app for $ENV..."
+fi
+
+cp "${PROJECT_DIR}/environment/${ENV}.env" "${PROJECT_DIR}/.env" 
