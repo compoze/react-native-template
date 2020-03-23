dc356df4433654a1ffd576f461e2e0e323c95116
spawn-dev <spawn-dev>
2020-03-22 19:57:39 -0500

    update to AUTH_CONFIG

--- scripts/appcenter_decode.sh
+++ scripts/appcenter_decode.sh
@@ -5,9 +5,9 @@ set -eo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_DIR="$SCRIPT_DIR/.."
 
-if [[ -z "${FIREBASE_CONFIG}" ]]; then
+if [[ -z "${AUTH_CONFIG}" ]]; then
   echo "Not in appcenter, ignoring..."
 else
   echo "In appcenter, decoding configuration"
-  echo "${FIREBASE_CONFIG}" | base64 --decode > "$PROJECT_DIR/serviceAccountKey.json"
+  echo "${AUTH_CONFIG}" | base64 --decode > "$PROJECT_DIR/serviceAccountKey.json"
 fi
