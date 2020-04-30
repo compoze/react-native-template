795a3151f743233cdb41593a6de62b45e03500e7
spawn-dev <spawn-dev>
2020-04-29 21:58:22 -0500

    add location service

--- src/services/LocationService.ts
+++ src/services/LocationService.ts
@@ -1,26 +1,42 @@
 // import { BASE_URL } from 'react-native-dotenv'
 import { Location } from '../model/Location';
+import { ApiService } from './ApiService';
+import { Auth } from '../stores/UserStore';
+
 // const BASE_API: string = BASE_URL;
 
 export class LocationService {
+    private apiService = new ApiService();
 
     public async getLocations(): Promise<Location[]> {
-        const locationA: Location =
-        {
-            latitude: 44.976400,
-            longitude: -93.268548,
-            description: "Customer location A",
-            name: "Custom A"
+        const query: string = `
+        query { getLocations(
+            input: ""
+          ) {
+                locations{
+                    id
+                    latitude
+                    longitude
+                    name
+                    description
+                },
+                errors{
+                    message
+                }
+            }
         }
-        const locationB: Location =
-        {
-            latitude: 44.986400,
-            longitude: -93.368548,
-            description: "Customer location B",
-            name: "Custom B"
-        }
+      `;
 
-        return [locationA, locationB];
+        try {
+            const response = await this.apiService.authenticatedGqlQuery(query);
+            if (response.getLocations.locations === null || response.getLocations.errors.length > 0) {
+                throw (response.getLocations.errors);
+            }
+            const locations: Location[] = response.getLocations.locations;
+            return locations;
+        } catch (error) {
+            throw (error);
+        }
     }
 
 }
