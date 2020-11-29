3008e7209726d33e341b77adb8167c5d4f84044e
Charlie Porth <charlieporth1@gmail.com>
2020-11-29 13:16:49 -0600

    Added custom butons

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -12,8 +12,10 @@ import { styleConstants } from '../../config/constants';
 export interface ButtonProps extends TouchableOpacityProperties {
   contentContainerStyle?: ViewStyle;
   invalid: boolean;
+  rounderCorners?:boolean | number;
+  hasShadow?: boolean;
 }
-
+const defaultRoundedCornerRadius = 2;
 const Button: React.SFC<ButtonProps> = ({
   style,
   contentContainerStyle,
@@ -24,6 +26,16 @@ const Button: React.SFC<ButtonProps> = ({
   if (props.invalid) {
     buttonStyles.push(styles.invalid);
   }
+  if (props.hasShadow || false) { // Change false if you want the default to use shadows -- Charlie Porth
+    buttonStyles.push(styles.shadow);
+  }
+  if (!!props.rounderCorners || false) { // Change false if you want the default to use roundered Corners -- Charlie Porth
+    if (typeof props.rounderCorners === 'number')
+      styles.defaultCorner.borderRadius = props.rounderCorners;
+
+    buttonStyles.push(styles.defaultCorner);
+  }
+
   return (
     <TouchableOpacity {...props} style={[...buttonStyles, style]}>
       <View style={contentContainerStyle}>{children}</View>
@@ -41,12 +53,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginVertical: 50,
-    borderRadius: 2,
-    shadowOffset: { width: 0, height: 2 },
-    shadowColor: 'black',
-    shadowOpacity: 0.14,
+
   },
   invalid: {
     backgroundColor: styleConstants.colors.PRIMARY_BUTTON_DISABLED,
   },
+  defaultCorner: {
+    borderRadius: defaultRoundedCornerRadius,
+  },
+  shadow: {
+    shadowOffset: { width: 0, height: 2 },
+    shadowColor: 'black',
+    shadowOpacity: 0.14,
+  }
 });
