d00792dda52a732e97c7a924b3f1f7cb25362e36
Charlie Porth <charlieporth1@gmail.com>
2020-12-08 09:11:58 -0600

    Small revisions

--- src/utilities/Logger.ts
+++ src/utilities/Logger.ts
@@ -18,29 +18,27 @@ export default class Log {
   private readonly isLoggingEnabled: boolean =
     env.toString().toLowerCase() === 'dev';
   //Debug log
-  public debug = (...msg: LogType[]) => {
+  public debug = (...msg: LogType[]): void => {
     if (this.isLoggingEnabled) {
       console.debug(JSON.stringify(`DEBUG: ${new Date()}: `, ...msg));
     }
   };
   //error log
-  public err = (...msg: LogType[] | Error[]) => {
+  public err = (...msg: LogType[] | Error[]): void => {
     if (this.isLoggingEnabled) {
       console.error(JSON.stringify(`ERROR: ${new Date()}: `, ...msg));
     }
   };
   //Alias
-  public error = (...msg: LogType[] | Error[]) => {
-    this.err(msg);
-  };
+  public error = (...msg: LogType[] | Error[]): void => this.err(msg);
   //log log
-  public log = (...msg: LogType[]) => {
+  public log = (...msg: LogType[]): void => {
     if (this.isLoggingEnabled) {
       console.log(JSON.stringify(`LOG: ${new Date()}: `, ...msg));
     }
   };
   //info log
-  public info = (...msg: LogType[]) => {
+  public info = (...msg: LogType[]): void => {
     if (this.isLoggingEnabled) {
       console.info(JSON.stringify(`INFO: ${new Date()}: `, ...msg));
     }
