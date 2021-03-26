d84fc8ef6bf0523e97b64ca412339fbd588793c3
larse514 <andrew.larse514@gmail.com>
2021-03-25 20:10:11 -0500

    update depdendencies

--- create.config.files.js
+++ create.config.files.js
@@ -16,7 +16,7 @@ try {
     }
 }
 catch (error) {
-    console.log("Error generation Firebase config files for IOS and Android.  Ensure that you have downloaded the serviceAccountKey.json file from https://console.firebase.google.com/project/spawnmyapp/settings/serviceaccounts/adminsdk  it is at the root of your project directory, and named serviceAccountKey.json")
+    console.log("Error generation Firebase config files for IOS and Android.  Ensure that you have downloaded the serviceAccountKey.json file from https://console.firebase.google.com/project/myapp/settings/serviceaccounts/adminsdk  it is at the root of your project directory, and named serviceAccountKey.json")
     process.exit(1);
 }
 
